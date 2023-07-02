9c8202f6d58595ca6d756c21d1ec42626f1a74bc
Martynas-J <Aurimhas@gmail.com>
2023-07-02 20:37:33 +0300

    CV

---
+++ src/Cv.js
@@ -0,0 +1,120 @@
+import { CV_DATA } from "./Data";
+import "./Cv.scss"
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import 'react-range-slider-input/dist/style.css';
+import  Container  from "./Components/Container/Container"
+
+function Cv() {
+  const { name, tel, email, address, aboutMe, links, hobbies, driverLicenses, personInfo, experience, education, skills, courses } = CV_DATA
+
+  const contactsElement = tel || email || address ?
+    <div className="person-data-box">
+      <h2 className="title">KONTAKTAI</h2>
+      {tel ? <span className="person-data-content"><span className="bold">Telefono Numeris:</span>{tel}</span> : ""}
+      {email ? <span className="person-data-content"><span className="bold">El.paštas:</span>{email}</span> : ""}
+      {address ? <span className="person-data-content"><span className="bold">Adresas:</span>{address}</span> : ""}
+    </div> : ""
+  const aboutMeElement =
+    <div className="person-data-box">
+      <h2 className="title">APIE MANE</h2>
+      <span className="person-data-content">{aboutMe}</span>
+    </div>
+
+  const linksElement = links ?
+    <div className="person-data-box">
+      <h2 className="title">NUORODOS</h2>
+      {links.map((link, index) =>
+        < Router key={index}>
+          <Link className="link" target="blank" to={link.url}><span className="bold">{link.name}:</span>{link.url}</Link>
+        </Router>
+      )}
+    </div > : ""
+
+  const hobbiesElement = hobbies ?
+    <div className="person-data-box">
+      <h2 className="title">POMĖGIAI</h2>
+      <span>{hobbies.join(", ")}</span>
+    </div > : ""
+
+  const driverLicensesElement = driverLicenses ?
+    <div className="person-data-box">
+      <h2 className="title">VAIRUOTOJO PAŽYMĖJIMAS</h2>
+      <span>
+        <span className="bold">Vairuotojo pažymėjimo {driverLicenses.length > 1 ? "kategorijos" : "kategorija"}
+        </span>{driverLicenses.join(", ")}
+      </span>
+    </div > : ""
+
+  const personInfoElement = personInfo ?
+    <div className="person-data-box">
+      <h2 className="title">ASMENINĖ INFORMACIJA</h2>
+      {personInfo.map((item, index) =>
+        <span key={index} className="person-data-content"><span className="bold">{item.title}</span>{item.value}</span>
+      )}
+    </div > : ""
+
+  const experienceElement = experience ?
+    <div className="person-data-box">
+      <h2 className="title line">DARBO PATIRTIS</h2>
+      <span>{experience}</span>
+    </div > : ""
+
+  const educationElement = education ?
+    <div className="person-data-box split">
+      <h2 className="title line">IŠSILAVINIMAS</h2>
+      {education.map((item, index) =>
+        <div key={index} className="person-data-content odd">
+          <span className="bold">{item.title}</span>
+          {item.place}, {item.institution} {item.year}
+          <p>{item.describe}</p>
+        </div>
+      )}
+    </div > : ""
+
+  const skillsElement = skills ?
+    <div className="person-data-box split">
+      <h2 className="title line">ĮGUDŽIAI</h2>
+      {skills.map((skill, index) =>
+        <div key={index} className="person-data-content">
+          <span className="bold">{skill.title}</span>
+          <input type="range" value={skill.lvl} max="10" readOnly></input>
+        </div>
+      )}
+    </div > : ""
+
+const coursesElement = courses ?
+<div className="person-data-box split">
+  <h2 className="title line">KURSAI</h2>
+  {courses.map((course, index) =>
+    <div key={index} className="person-data-content">
+      <span className="person-data-content"><span className="bold">{course.name}</span>{course.institution} {course.startDate} - {course.endDate}</span>
+    </div>
+  )}
+</div > : ""
+
+  return (
+    <Container>
+      <header>
+        {name}
+      </header>
+      <div className="wrapper">
+        <aside className="sidebar-content">
+          {contactsElement}
+          {aboutMe && aboutMeElement}
+          {linksElement}
+          {hobbiesElement}
+          {driverLicensesElement}
+          {personInfoElement}
+        </aside>
+        <main className="main-content">
+          {experienceElement}
+          {educationElement}
+          {skillsElement}
+          {coursesElement}
+        </main>
+      </div>
+    </Container>
+  )
+}
+
+export default Cv;
---
+++ src/Data.js
@@ -0,0 +1,81 @@
+export const CV_DATA = {
+    name: "Martynas Janušauksas",
+    tel: "+37064191986",
+    email: "janusauskas.martynas1@gmail.com",
+    address: "Jūros gatvė 6, Šaukų mst., Kelmės raj., Lietuva, 86383",
+    aboutMe: "Esu pareigingas, sąžiningas, nuoširdus, tvarkingas. Sugebu greitai perprasti naujas užduotis. Moku dirbti ir komandoje, ir individualiai.",
+    links: [
+        {
+            name: "LinkedIn",
+            url: "https://linkedin.com/in/martynas-janusauskas-b4381658",
+        },
+        {
+            name: "GitHub",
+            url: "https://github.com/Martynas-J",
+        }
+    ],
+    hobbies: ["Krepšinis", "Tinklinis"],
+    driverLicenses: ["B"],
+    personInfo: [
+        {
+            title: "Gimimo data",
+            value: "1990-11-02",
+        },
+        {
+            title: "Tautybė",
+            value: "Lietyvis",
+        }, {
+            title: "Šeimyninė padėtis",
+            value: "Vedęs",
+        }
+    ],
+    experience: "-",
+    education: [
+        {
+            title: "Bakalauras",
+            place: "Šiauliai",
+            institution: "Šiaulių universitetas",
+            year: "2014",
+            describe: "Baigiau informatikos inžinerijos bakalauro studijas Šiaulių universitete. 2009-2014"
+        },
+        {
+            title: "Kursai",
+            place: "Online",
+            institution: "CodeAcademy",
+            year: "2023",
+            describe: "Užbaigtas 3 mėn. intensyvus Front-End (JavaScript, React) kursas, kurio metu buvo mokomasi HMTL/CSS, SCSS (Sass CSS) bei susipažinta su Bootstrap biblioteka. 2023"
+        },
+
+    ],
+    skills: [
+        {
+            title: "HTML",
+            lvl: 7,
+        },
+        {
+            title: "CSS",
+            lvl: 7,
+        },
+        {
+            title: "SCSS",
+            lvl: 7,
+        },
+        {
+            title: "JavaScript",
+            lvl: 6,
+        },
+        {
+            title: "REACT",
+            lvl: 6,
+        },
+    ],
+    courses: [
+        {
+            name: "FRONT-END",
+            institution: "CodeAcademy",
+            startDate: "2023 kov.",
+            endDate: "2023 bir."
+        }
+    ]
+
+}
